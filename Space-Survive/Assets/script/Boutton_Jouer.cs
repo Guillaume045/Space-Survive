8e532b38f31bbdc4881c5d938fc4bc7ff686fdcc
lguillau <guillaume.lanfroid@ynoc.com>
2024-01-11 09:02:54 +0100

    jeux

---
+++ Space-Survive/Assets/script/Boutton_Jouer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Boutton_Jouer : MonoBehaviour
+{
+    public Button votreBouton;
+
+    void Start()
+    {
+        votreBouton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("Le bouton a été cliqué");
+        SceneManager.LoadScene(0);
+        Debug.Log("Vous avez changer de scene");
+    }
+}
